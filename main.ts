e81b423f2b366482662e4fe3470f9289b379d032
Asra Urooj <160761127+Asraurooj@users.noreply.github.com>
2024-04-17 06:33:27 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+console.log(chalk.blue.bold("\n \t Wellcome to 'codewithasra' - currency converter\n"));
+
+//Define the list of currencies and their exchange rates
+let exchange_rate: any = {
+    "USD": 1,  //Base Currency
+    "EUR": 0.9,  //European Currency(Euro)
+    "JYP": 113,  //Japnese Currency(Yen)
+    "CAD": 1.3,  //Canadian Dollar
+    "AUD": 1.65,  //Australian Dollar
+    "PKR": 277.70,  //Pakistan Rupees
+    //Add more currencies and their exchange rates here
+} 
+
+//Prompt the user to select currencies to connect from and to
+let user_answers = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: "Select tht currency to convert from:",
+        choices: [ "USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: "Select the currency to convert into:",
+        choices: [ "USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+
+    },
+    {
+        name: "amount",
+        type: "input",
+        message: "Enter the amount to convert:"
+    }
+]);
+
+//Perform currency converter by using formula
+let from_amount = exchange_rate[user_answers.from_currency];
+let to_amount = exchange_rate[user_answers.to_currency];
+let amount = user_answers.amount
+
+//formula ofconversion
+let base_amount = amount / from_amount
+let converted_amount = base_amount * to_amount
+
+//Display the converted amount
+console.log(`Converted Amount = ${chalk.green(converted_amount.toFixed(2))}`);
